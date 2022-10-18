6c4136fd2f661d62ac21e555c528cc410f11b27d
Semih Gokol <gokol.semih@gmail.com>
2022-10-18 09:47:38 +0200

    factiorialNumber challenge done

---
+++ factioralNumber.js
@@ -0,0 +1,23 @@
+/*
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Only integers greater than or equal to zero will be supplied to the function.
+*/
+
+function factorialize(num) {
+    // Si nbr = 0 la factorielle retournera 1
+    if (num === 0)
+    {
+       return 1;
+    }
+    // appelez à nouveau la procédure récursive
+    return num * factorialize(num-1);
+  }
+  
+  factorialize(5);
