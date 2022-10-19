24b40389548d54c26c94bc92a62e2a6fbc469a83
Semih Gokol <gokol.semih@gmail.com>
2022-10-19 16:05:58 +0200

    longest word in a string challenge done

---
+++ LongestWord.js
@@ -0,0 +1,13 @@
+/*
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+*/
+
+function findLongestWordLength(s) {
+  return s.split(' ')
+    .reduce(function(longest, word) {
+      return Math.max(longest, word.length)
+    }, 0);
+}
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
